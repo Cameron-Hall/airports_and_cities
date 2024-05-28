b7557e1d75c89e674829240628fa2fde66126380
Cameron Hall <22chall@wakatipu.school.nz>
2024-05-28 13:23:33 +1200

    Connection + initial information

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 import sqlite3
 
+with sqlite3.connect('europe_airpots.db') as conn:
+    cursor = conn.cursor()
+    user_choice = input("What would you like to find information about?\n1. Airports\n2. Cities\n> ")
+
+    conn.commit()
